0e56dac607fe307686767ad780fa5d0d923b0850
Benedek Horváth <benedekhorvath@edu.bme.hu>
2026-03-24 19:23:41 +0000

    init CI/CD setup

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return "OK"
+
+@app.route("/")
+def index():
+    return "OCR App - Under Construction"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
